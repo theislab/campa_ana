824050cec0d5e6df77aa35fd3a7f84147a23c0a0
scottberry <scottdberry@gmail.com>
2022-03-22 11:45:54 +0000

    ggsave_cairo added

--- R/io.R
+++ R/io.R
@@ -4,3 +4,52 @@ ggsave_cairo <- function(filename,plot = last_plot(),width=NA,height=NA,units="c
   print(plot)
   dev.off()
 }
+
+load_channels <- function(data_dir) {
+  channels <- read_csv(file.path(data_dir,"channels.csv"),col_names = F,show_col_types = F)
+  names(channels) <- c("index","channel_name")
+  return(channels$channel_name)
+}
+
+load_input_data <- function(dirname,channels) {
+  mapobject_ids <- np$load(file.path(dirname,"obj_ids.npy"))
+  labels <- np$load(file.path(dirname,"labels.npy"))
+  x <- np$load(file.path(dirname,"x.npy"))
+  y <- np$load(file.path(dirname,"y.npy"))
+  
+  mpp <- np$load(file.path(dirname,"mpp.npy"))
+  colnames(mpp) <- channels
+  combined <- tibble(mapobject_id=mapobject_ids,
+                     x=x,
+                     y=y) %>%
+    bind_cols(as_tibble(mpp))
+  return(combined)
+}
+
+load_ilastik_data <- function(dirname,channels) {
+  mapobject_ids <- np$load(file.path(dirname,"mapobject_ids.npy"))
+  labels <- np$load(file.path(dirname,"labels.npy"))
+  x <- np$load(file.path(dirname,"x.npy"))
+  y <- np$load(file.path(dirname,"y.npy"))
+  
+  mpp <- np$load(file.path(dirname,"mpp.npy"))
+  colnames(mpp) <- channels
+  combined <- tibble(mapobject_id=mapobject_ids,
+                     x=x,
+                     y=y) %>%
+    bind_cols(as_tibble(mpp))
+  return(combined)
+}
+
+load_pixel_clustering <- function(dirname,filename) {
+  mapobject_ids <- np$load(file.path(dirname,"obj_ids.npy"))
+  cluster_ids <- np$load(file.path(dirname,filename))
+  x <- np$load(file.path(dirname,"x.npy"))
+  y <- np$load(file.path(dirname,"y.npy"))
+  
+  combined <- tibble(mapobject_id=mapobject_ids,
+                     x=x,
+                     y=y,
+                     cluster_id=cluster_ids) 
+  return(combined)
+}
